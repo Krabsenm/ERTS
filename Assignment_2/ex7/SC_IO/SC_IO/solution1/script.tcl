d58bb174fe5be8812db782349e5bbdd2adcc3a02
Mads Krabsen <krabsenm@gmail.com>
2019-10-15 11:22:18 +0200

    ex7 stuff

--- Assignment_2/ex7/SC_IO/SC_IO/solution1/script.tcl
+++ Assignment_2/ex7/SC_IO/SC_IO/solution1/script.tcl
@@ -5,11 +5,11 @@
 ############################################################
 open_project SC_IO
 set_top iosc
-add_files ios.h
 add_files ios.cpp
-add_files -tb tb_ios.cpp
-add_files -tb ios_driver.h
+add_files ios.h
 add_files -tb ios_driver.cpp
+add_files -tb ios_driver.h
+add_files -tb tb_ios.cpp
 open_solution "solution1"
 set_part {xc7z020clg484-1}
 create_clock -period 10 -name default
